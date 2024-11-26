4568bb6ce9527de59c79bfe797a9c09d2fae639b
Aluno <thonne.cleiton@gmail.com>
2024-11-25 21:15:19 -0300

    backend API

---
+++ Task/Controllers/TaskControler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+
+    public class TaskControler : ControllerBase
+    {
+        private static List<Model_Task> model_Tasks = 
+            new List<Model_Task>();
+
+        [HttpGet]
+        public ActionResult<List<Model_Task>> SearchTask()
+        {
+            return Ok(model_Tasks);
+        }
+
+        [HttpPost]
+        public ActionResult AddTask( Model_Task newTask)
+        {
+            model_Tasks.Add(newTask);
+            return Ok(model_Tasks);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteTask(int Id) {
+            {
+                model_Tasks.RemoveAt(Id);
+                return Ok(model_Tasks);
+            }
+    }}
+}
---
+++ Task/Models/ModelTask.cs
@@ -0,0 +1,10 @@
+﻿namespace Task.Models
+{
+    public class Model_Task
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+
+}
